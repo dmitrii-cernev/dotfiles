013213f99a5fa906a6242f4e279dacddf86b40f4
dmitriicernev <dmitrii.cernev@piercegroup.com>
2025-05-29 21:10:34 +0200

    add nvim config

---
+++ nvim/.config/nvim/lua/config/basic.lua
@@ -0,0 +1,53 @@
+
+-- Enable line numbers
+vim.opt.number = true
+
+-- Enable relative line numbers
+vim.opt.relativenumber = true
+
+-- Enable syntax highlighting and filetype detection
+vim.cmd('syntax on')
+vim.cmd('filetype plugin indent on')
+
+-- Tabs and indentation
+vim.opt.tabstop = 4        -- Number of spaces that a <Tab> counts for
+vim.opt.shiftwidth = 4     -- Number of spaces to use for autoindent
+vim.opt.expandtab = true   -- Use spaces instead of tabs
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+
+-- Search settings
+vim.opt.hlsearch = true        -- Highlight search results
+vim.opt.incsearch = true       -- Show matches as you type
+vim.opt.ignorecase = true      -- Case-insensitive search...
+vim.opt.smartcase = true       -- ...unless capital letters are used
+
+-- Show matching parentheses/brackets
+vim.opt.showmatch = true
+
+-- Display command being typed
+vim.opt.showcmd = true
+
+-- Use system clipboard if available
+vim.opt.clipboard = 'unnamedplus'
+
+-- Allow backspacing over everything in insert mode
+vim.opt.backspace = { 'indent', 'eol', 'start' }
+
+-- Autohide command line when not in use
+-- hide command-line when unused
+vim.o.cmdheight = 0
+
+-- make cmdheight 1 when you enter : or /
+vim.api.nvim_create_autocmd({ "CmdlineEnter", "CmdlineChanged", "CmdlineLeave" }, {
+  callback = function(event)
+    if event.event == "CmdlineEnter" then
+      vim.o.cmdheight = 1
+    elseif event.event == "CmdlineLeave" then
+      vim.o.cmdheight = 0
+    end
+  end,
+})
+
+-- optional: disable built-in showcmd (you get partial keys in the statusline already)
+vim.o.showcmd = false
---
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,20 @@
+return {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim",
+    },
+    config = function()
+        vim.keymap.set("n", "<leader>e", function()
+            if vim.bo.filetype == "neo-tree" then
+                -- if we're already in Neo-tree, close it (focus returns to the file)
+                vim.cmd("Neotree close")
+            else
+                -- otherwise open (or focus) the filesystem tree and reveal the current file
+                vim.cmd("Neotree source=filesystem reveal=true position=left")
+            end
+        end, { silent = true, noremap = true })
+    end,
+}
